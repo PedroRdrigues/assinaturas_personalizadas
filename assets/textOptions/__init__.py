1af3d9b1b0dbb7eb3fd6df99f712237148be09ee
PedroRdrigues <p.hrdrigues1@gmail.com>
2025-02-15 11:39:23 -0300

    Exclusão de arquivos de posicionamento de texto

--- assets/textOptions/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb  4 16:45:38 2025
-
-@author: moto-ti
-"""
-
---
+++ interface.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 15 11:09:54 2025
+
+@author: moto-ti
+"""
+
+""" CRIAR UMA INTERFACE QUE POSSA SER UTILIZADA PARA A CONFIGURAÇÃO DA ASSINATURA DO COLABORADOR """
